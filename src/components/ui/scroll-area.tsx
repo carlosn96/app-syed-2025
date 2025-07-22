8316fedda4351757f079ebccd4dc99e7f1073b5f
ANGEL YIREH GARCIA JARA <2202087@universidad-une.com>
2025-07-22 23:17:53 +0000

    Ahora las scrollbar no estan con el degradado del header

--- src/components/ui/scroll-area.tsx
+++ src/components/ui/scroll-area.tsx
@@ -40,7 +40,7 @@ const ScrollBar = React.forwardRef<
     )}
     {...props}
   >
-    <ScrollAreaPrimitive.ScrollAreaThumb className="relative flex-1 rounded-full login-background" />
+    <ScrollAreaPrimitive.ScrollAreaThumb className="relative flex-1 rounded-full header-background" />
   </ScrollAreaPrimitive.ScrollAreaScrollbar>
 ))
 ScrollBar.displayName = ScrollAreaPrimitive.ScrollAreaScrollbar.displayName
