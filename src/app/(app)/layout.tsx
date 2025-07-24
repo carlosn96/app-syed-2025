861443d7c5c908eae316a7faef2db2f26b6a1209
ANGEL YIREH GARCIA JARA <2202087@universidad-une.com>
2025-07-24 18:53:30 +0000

    Make the following changes:
    ,
    
    Find this element:
    
      <main class="flex-

--- src/app/(app)/layout.tsx
+++ src/app/(app)/layout.tsx
@@ -47,7 +47,7 @@ export default function AppLayout({ children }: { children: React.ReactNode }) {
               <UserNav />
             </div>
           </header>
-          <main className="flex-1 overflow-y-auto p-4 md:p-8 glass-card">
+          <main className="flex-1 overflow-y-auto p-4 md:p-8 bg-background">
             {children}
           </main>
         </SidebarInset>
