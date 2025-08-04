b3e97a9c3bc335be545ef3d350e9d01f951fe02b
ANGEL YIREH GARCIA JARA <2202087@universidad-une.com>
2025-08-04 17:45:08 +0000

    Puedes hacer un pco mas grande los checkbox para que sea mas sencillo de

--- src/components/ui/checkbox.tsx
+++ src/components/ui/checkbox.tsx
@@ -14,7 +14,7 @@ const Checkbox = React.forwardRef<
   <CheckboxPrimitive.Root
     ref={ref}
     className={cn(
-      "peer h-4 w-4 shrink-0 rounded-sm border border-primary ring-offset-background focus-visible:outline-none focus-visible:ring-2 focus-visible:ring-ring focus-visible:ring-offset-2 disabled:cursor-not-allowed disabled:opacity-50 data-[state=checked]:bg-destructive data-[state=checked]:text-destructive-foreground",
+      "peer h-5 w-5 shrink-0 rounded-sm border border-primary ring-offset-background focus-visible:outline-none focus-visible:ring-2 focus-visible:ring-ring focus-visible:ring-offset-2 disabled:cursor-not-allowed disabled:opacity-50 data-[state=checked]:bg-destructive data-[state=checked]:text-destructive-foreground",
       className
     )}
     {...props}
@@ -22,7 +22,7 @@ const Checkbox = React.forwardRef<
     <CheckboxPrimitive.Indicator
       className={cn("flex items-center justify-center text-current")}
     >
-      <Check className="h-4 w-4" />
+      <Check className="h-5 w-5" />
     </CheckboxPrimitive.Indicator>
   </CheckboxPrimitive.Root>
 ))
