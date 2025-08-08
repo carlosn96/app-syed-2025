20b7ec68bd9b77306c2b71baac6aec9e209c4279
ANGEL YIREH GARCIA JARA <2202087@universidad-une.com>
2025-08-08 20:53:01 +0000

    Make the following changes:
    ,
    
    Find this element:
    
      <SidebarLayout>
    
    w

--- src/components/ui/sidebar.tsx
+++ src/components/ui/sidebar.tsx
@@ -194,15 +194,7 @@ const Sidebar = React.forwardRef<
     const { isMobile, openMobile, setOpenMobile, state } = useSidebar()
     const isCollapsed = state === 'collapsed';
 
-    const renderContent = () => (
-      <SidebarBody>
-        <SidebarHeader />
-        <SidebarContent>
-            {children}
-        </SidebarContent>
-        <SidebarFooter/>
-      </SidebarBody>
-    );
+    const renderContent = () => children;
 
     if (isMobile) {
       return (
@@ -370,7 +362,7 @@ const SidebarMenuItem = React.forwardRef<
 SidebarMenuItem.displayName = "SidebarMenuItem"
 
 const sidebarMenuButtonVariants = cva(
-  "peer/menu-button flex w-full items-center gap-3 rounded-full text-left outline-none ring-sidebar-ring transition-all duration-300 focus-visible:ring-2 active:opacity-80 disabled:pointer-events-none disabled:opacity-50 group-has-[[data-sidebar=menu-action]]/menu-item:pr-8 aria-disabled:pointer-events-none aria-disabled:opacity-50 group-data-[state=expanded]:px-3 group-data-[state=collapsed]:w-11 group-data-[state=collapsed]:justify-center [&>span]:group-data-[state=collapsed]:w-0 [&>span]:group-data-[state=collapsed]:opacity-0 [&>svg]:size-5 [&>svg]:shrink-0",
+  "peer/menu-button flex w-full items-center rounded-full text-left outline-none ring-sidebar-ring transition-all duration-300 focus-visible:ring-2 active:opacity-80 disabled:pointer-events-none disabled:opacity-50 group-has-[[data-sidebar=menu-action]]/menu-item:pr-8 aria-disabled:pointer-events-none aria-disabled:opacity-50 group-data-[state=expanded]:px-3 group-data-[state=expanded]:gap-3 group-data-[state=collapsed]:w-11 group-data-[state=collapsed]:justify-center group-data-[state=collapsed]:p-0 [&>span]:group-data-[state=collapsed]:w-0 [&>span]:group-data-[state=collapsed]:opacity-0 [&>svg]:size-5 [&>svg]:shrink-0",
   {
     variants: {
       variant: {
