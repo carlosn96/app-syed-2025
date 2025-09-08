59a17bdfa8dc15653b6301262aeb29a15ae3b1ab
ANGEL YIREH GARCIA JARA <2202087@universidad-une.com>
2025-09-08 19:24:14 +0000

    Arregla este toast, ya que deberia de ser verde y verse mas bonito (_for

--- src/components/ui/badge.tsx
+++ src/components/ui/badge.tsx
@@ -17,7 +17,7 @@ const badgeVariants = cva(
           "border-transparent bg-destructive text-destructive-foreground hover:bg-destructive/80",
         outline: "text-foreground",
         success: 
-          "border-transparent bg-green-600 text-white",
+          "border-transparent bg-success text-white",
         warning: 
           "border-transparent bg-yellow-500 text-black",
         info:
