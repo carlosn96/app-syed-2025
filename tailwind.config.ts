c43946bf14fad3d03ff3567460b63c7e18a9a046
ANGEL YIREH GARCIA JARA <2202087@universidad-une.com>
2025-11-03 20:21:07 +0000

    Donde se ve la dispocision de cada tamaño de pantalla, si despues quicie

--- tailwind.config.ts
+++ tailwind.config.ts
@@ -17,6 +17,25 @@ export default {
       },
     },
     extend: {
+      screens: {
+        'sm': '640px',
+        // => @media (min-width: 640px) { ... }
+  
+        'md': '768px',
+        // => @media (min-width: 768px) { ... }
+  
+        'lg': '1024px',
+        // => @media (min-width: 1024px) { ... }
+  
+        'xl': '1280px',
+        // => @media (min-width: 1280px) { ... }
+  
+        '2xl': '1536px',
+        // => @media (min-width: 1536px) { ... }
+
+        // Aquí podrías añadir o modificar breakpoints
+        // '3xl': '1920px', 
+      },
       fontFamily: {
         body: ['Inter', 'sans-serif'],
         headline: ['Space Grotesk', 'sans-serif'],
