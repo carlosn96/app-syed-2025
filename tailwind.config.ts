215d9fb02e925bf2ec3ab36549bbdb77fe54c80a
ANGEL YIREH GARCIA JARA <2202087@universidad-une.com>
2025-11-03 21:25:11 +0000

    donde se ve la configuracion del boton, cuando las pantallas son mas chi

--- tailwind.config.ts
+++ tailwind.config.ts
@@ -21,10 +21,11 @@ export default {
         'sm': '640px',
         // => @media (min-width: 640px) { ... }
   
-        'md': '768px',
+        //'md': '768px',
+        'md': '1024px',
         // => @media (min-width: 768px) { ... }
   
-        'lg': '1024px',
+        //'lg': '1024px',
         // => @media (min-width: 1024px) { ... }
   
         'xl': '1280px',
